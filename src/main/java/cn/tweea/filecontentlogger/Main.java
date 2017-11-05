19e8960427d4d5fcbfc786cf59537beda6aa7b84
Tweea <tweea@263.net>
2017-11-06 00:44:13 +0800

    参数处理

---
+++ src/main/java/cn/tweea/filecontentlogger/Main.java
@@ -0,0 +1,53 @@
+/*
+ * 版权所有 2017 Tweea。
+ * 保留所有权利。
+ */
+package cn.tweea.filecontentlogger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.WritableResource;
+
+public final class Main {
+	public static void main(String[] args)
+		throws IOException {
+		if (args.length < 3) {
+			throw new IllegalArgumentException("缺少参数");
+		}
+
+		Resource inputResource;
+		if ("-".equals(args[0])) {
+			inputResource = null;
+		} else {
+			inputResource = new FileSystemResource(args[0]);
+			if (!inputResource.exists()) {
+				throw new IllegalArgumentException("输入文件不存在");
+			}
+		}
+		File fileBase = new File(args[1]);
+		if (!fileBase.exists()) {
+			throw new IllegalArgumentException("数据目录/文件不存在");
+		}
+		WritableResource outputResource = new FileSystemResource(args[2]);
+		if (outputResource.exists()) {
+			System.out.print("输出文件已存在，是否覆盖？");
+			char answer = (char) System.in.read();
+			if (answer != 'Y' && answer != 'y') {
+				return;
+			}
+		}
+
+		Map<String, FileContent> fileContents;
+		if (inputResource == null) {
+			fileContents = new TreeMap<>();
+		} else {
+			fileContents = DataFiles.readFileContentLog(inputResource);
+		}
+		DataFiles.writeFileContentLog(fileContents, outputResource);
+	}
+}

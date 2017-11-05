f63f626f68b3a1727d45cbce04a0e78e119b2938
Tweea <tweea@263.net>
2017-11-05 23:45:32 +0800

    文件读写

---
+++ src/main/java/cn/tweea/filecontentlogger/DataFiles.java
@@ -0,0 +1,56 @@
+/*
+ * 版权所有 2017 Tweea。
+ * 保留所有权利。
+ */
+package cn.tweea.filecontentlogger;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.WritableResource;
+import org.yaml.snakeyaml.Yaml;
+
+public class DataFiles {
+	private static Yaml YAML = new Yaml();
+
+	public static void writeFileContentLog(Map<String, FileContent> fileContents, WritableResource target)
+		throws IOException {
+		try (PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(target.getOutputStream(), StandardCharsets.UTF_8)))) {
+			for (FileContent fileContent : fileContents.values()) {
+				writer.println('-');
+				writer.print("  hash: '");
+				writer.print(fileContent.getHash());
+				writer.println('\'');
+				writer.print("  length: ");
+				writer.println(fileContent.getLength());
+				writer.print("  name: '");
+				writer.print(fileContent.getName());
+				writer.println('\'');
+			}
+			writer.flush();
+		}
+	}
+
+	public static Map<String, FileContent> readFileContentLog(Resource source)
+		throws IOException {
+		Map<String, FileContent> fileContents = new TreeMap<>();
+
+		List<Map<String, Object>> fileContentMapList = YAML.load(source.getInputStream());
+		for (Map<String, Object> fileContentMap : fileContentMapList) {
+			FileContent fileContent = new FileContent();
+			fileContent.setHash((String) fileContentMap.get("hash"));
+			fileContent.setLength(((Number) fileContentMap.get("length")).longValue());
+			fileContent.setName((String) fileContentMap.get("name"));
+			fileContents.put(fileContent.getHash(), fileContent);
+		}
+
+		return fileContents;
+	}
+}
---
+++ src/main/java/cn/tweea/filecontentlogger/FileContent.java
@@ -0,0 +1,45 @@
+/*
+ * 版权所有 2017 Tweea。
+ * 保留所有权利。
+ */
+package cn.tweea.filecontentlogger;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class FileContent {
+	private String hash;
+
+	private long length;
+
+	private String name;
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+
+	public long getLength() {
+		return length;
+	}
+
+	public void setLength(long length) {
+		this.length = length;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE).append("hash", hash).append("length", length).append("name", name).toString();
+	}
+}
